bf4d576fb0818a1c10dc9d363f2fe53d7e9963a6
Andres <marulandaandres19@gmail.com>
2023-11-01 16:12:29 -0500

    Rename to JSX

--- App.jsx
+++ App.jsx
@@ -1,7 +1,11 @@
 import { SafeAreaView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
-import Welcome from "./screens/Welcome.js";
+import Welcome from "./screens/Welcome";
+import SignIn from "./screens/SignIn";
+import SignUp from "./screens/SignUp";
+import Home from "./screens/Home";
+import Create from "./screens/Create";
 
 export default function App() {
   return (
